c4aba3043439caab52f1f7573ffc0bc8bf85e01a
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-31 20:45:46 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.DPRO_CONFIG = Object.freeze({
+  systemCode: "CAREPLAN",
+  systemName: "DPRO 居宅介護支援・ケアマネ LINE",
+  version: "CAREPLAN-3-FRONT-20260731",
+  apiBase: "https://dpro-careplan-line-api.dpromstk2000.workers.dev",
+  organizationCode: "dpro_careplan_demo",
+  environment: "demo",
+  liffId: "",
+  demo: true
+});
